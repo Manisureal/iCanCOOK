c82e527c08329fcade7ef908b1a45eb3112433ad
MansoorZaman <manisureal@outlook.com>
2017-11-24 11:21:11 +0000

    Created a Seed File

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(email: "cook@cook.com", encrypted_password: "123456", first_name: "Han", surname: "solo")
+User.create!(email: "cook1@cook.com", encrypted_password: "123456", first_name: "Kane", surname: "Black")
+
+Listing.create!(User_id: 1, description: "I am an amazing cook... Trust me!", experience: "10 years", price: 10, speciality: "Birthdays and BBQs", line1: "138 Kingsland Avenue", city: "London", postcode: "EC1N3PU", bookings_completed: 5, dates: "2017-12-01", events: "BBQ" )
+Listing.create!(User_id: 2, description: "I am an amazing cook... Trust is the Key!", experience: "12 years", price: 12, speciality: "BBQs, Dinner Parties", line1: "145 Kingsland Avenue", city: "London", postcode: "EC1N3PU", bookings_completed: 4, dates: "2017-12-12", events: "BBQ" )
+
+Booking.create!(listing_id: 1, user_id: 1, booking_date: "Fri, 24 Nov 2017", total_price: 40, duration: 4, status: "pending")
+Booking.create!(listing_id: 2, user_id: 2, booking_date: "Fri, 24 Nov 2017", total_price: 50, duration: 5, status: "pending")
