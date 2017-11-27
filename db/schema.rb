89f5c7ec8b95504029d533d049da8e705d845094
Luka Prelevic <lukaprelevic@gmail.com>
2017-11-27 14:09:41 +0000

    Added code that had avatar problem fixes upon signup

--- db/schema.rb
+++ db/schema.rb
@@ -78,7 +78,7 @@ ActiveRecord::Schema.define(version: 20171127101344) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "surname"
-    t.string "avatar"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
