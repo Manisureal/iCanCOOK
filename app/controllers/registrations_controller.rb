a00acaa5b262c0759c42d62a871905039cfd54d2
Luka Prelevic <lukaprelevic@gmail.com>
2017-11-27 21:10:58 +0000

    created the code for dashboard without listing

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,16 @@
 class RegistrationsController < Devise::RegistrationsController
   def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :photo)
+    params.require(:user).permit(:first_name, :surname, :email, :password, :password_confirmation, :photo)
   end
+
+  def account_update_params
+    params.require(:user).permit(:first_name, :surname, :email, :password, :password_confirmation, :current_password, :photo)
+  end
+
+  private
+
+  def after_update_path_for(resource)
+    dashboard_path
+  end
+
 end
