a7a8a61dd6b6604bca55441ba1c3c703304c0230
MansoorZaman <manisureal@outlook.com>
2017-11-28 13:59:40 +0000

    Added Image Upload Functionality to Listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,7 +3,7 @@ class Listing < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
-  # mount_uploader :photos, PhotoUploader
+  mount_uploader :photos, PhotoUploader
 
   EVENTS = ["Canapes Parties", "BBQ", "Birthday Parties", "Dinner Parties", "Garden Parties", "Family Cooks"]
 end
