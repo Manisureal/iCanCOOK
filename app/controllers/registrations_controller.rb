b952995dee7a6d5394f8eb7537751bc4aff0beff
MansoorZaman <manisureal@outlook.com>
2017-11-28 11:56:25 +0000

    Removed Avatar from Users and replaced it with Photos

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
+  prepend_before_action :verify_signed_out_user, only: :destroy
+
   def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :photo)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :photo, :photo_cache)
   end
 end
