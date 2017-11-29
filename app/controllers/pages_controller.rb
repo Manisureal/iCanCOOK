27051e6b2183e37e3345bfa53efd491cb2cb3f0a
Luka Prelevic <lukaprelevic@gmail.com>
2017-11-29 16:38:19 +0000

    finally finished the dashboard with all the logic working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,7 +10,9 @@ class PagesController < ApplicationController
     @last_booking = Booking.where(listing_id: @user.id).last
     @nb_booking = Booking.where(listing_id: @user.id).length
     @listings = Listing.where(user: @user)
+    @listing = @listings.last
     @find_buyer = Booking.where(listing: @user).first.user if @find_buyer
+    @my_upcoming_appointments = Booking.where(listing_id: @user.id).all
   end
 
   def confirm
